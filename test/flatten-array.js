14bd8a7779e89eee083248968c0c404e470eae00
JacKhinFai <41749876+JacKhinFai@users.noreply.github.com>
2018-08-08 12:48:56 +0100

    Update flatten-array.js

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -1,7 +1,8 @@
 describe('flatten array', function () {
   it('should flatten an array', function () {
-    var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
-        expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
+    var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]];
+        //expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
+	var expected = [].concat.apply([], arr);
 
     expect(arr).toEqual(expected);
   });
